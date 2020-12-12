26a4ae87ac410140669017a9fd9e32b1260803e3
Oleg.L <testtesttest@test.ru>
2020-12-12 20:34:05 +0300

    code review for ViewBanner.vue and his relatives

--- src/components/banner/ViewBanner.vue
+++ src/components/banner/ViewBanner.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
+    <!-- banner container zoom tag -->
     <div
       :style="{ transform: `scale(${zoomModifier})`, transformOrigin: 'top' }"
     >
       <v-container class="d-flex justify-center align-center">
         <v-row class="d-flex justify-center">
           <v-col class="col-auto">
+            <!-- banner container -->
             <app-banner-container :zoomModifier="zoomModifier">
             </app-banner-container>
           </v-col>
         </v-row>
       </v-container>
     </div>
+    <!-- zoom banner slider -->
     <v-container class="app-main__scale">
       <v-row>
         <v-col>
@@ -19,12 +22,12 @@
             v-model="zoomModifier"
             append-icon="mdi-magnify-plus-outline"
             class=""
+            inverse-label
             max="1"
             min="0.1"
-            inverse-label
-            step="0.01"
             persistent-hint
             prepend-icon="mdi-magnify-minus-outline"
+            step="0.01"
             thumb-label
           ></v-slider>
         </v-col>
